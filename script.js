f8414b5ee4264b538b45523ff51be03d7fa0a06a
triumph11 <130575743+triumph11@users.noreply.github.com>
2024-07-25 18:26:41 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript is working!");
-});
-
-function callMe() {
-    window.location.href = "tel:+1234567890"; // Replace with your phone number
-}
