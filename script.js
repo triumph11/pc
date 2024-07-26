737ef342855db32119dea21ca7621a9bc24203b0
triumph11 <130575743+triumph11@users.noreply.github.com>
2024-07-25 18:27:08 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript is working!");
+});
+
+function callMe() {
+    window.location.href = "tel:+1234567890"; // Replace with your phone number
+}
